cde35a0211597bf3fd8101cf82cb770ee4144351
Ananya Sharma <as1109work@gmail.com>
2024-05-14 21:08:33 +0530

    Update ReverseString.c

--- String/ReverseString.c
+++ String/ReverseString.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-void ReverseString(char A[]){
+void ReverseString(char A[]){  //using an extra array
     char B[100];
     int i,j;
     for(i=0;A[i]!='\0';i++){
@@ -15,9 +15,24 @@ void ReverseString(char A[]){
     printf("%s",B);
 }
 
+void ReverseBySwapping(char A[]){
+    char temp;
+    int i,j;
+    for(j=0;A[j]!='\0';j++){
+    }
+    j=j-1;
+    for(i=0;i<j;i++,j--){
+        temp=A[i];
+        A[i]=A[j];
+        A[j]=temp;
+    }
+}
+
 int main(){
     char A[]="python";
     ReverseString(A);
+    printf("\n");
+    ReverseBySwapping(A);
 
     return 0;
 }
