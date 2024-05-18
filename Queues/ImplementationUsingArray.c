07f68a8130549943fa5c6c40cf7c94819a533570
Ananya Sharma <as1109work@gmail.com>
2024-05-18 11:22:57 +0530

    Create ImplementationUsingArray.c

---
+++ Queues/ImplementationUsingArray.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct queue{
+    int size;
+    int front;
+    int rear;
+    int *Q;
+};
+
+void create(struct queue *q,int size){
+    q->size=size;
+    q->front=q->rear=-1;
+    q->Q=(int *)malloc(q->size*sizeof(int));
+}
+
+void enqueue(struct queue *q,int x){
+    if(q->rear==q->size-1){
+        printf("queue is ful");
+    }
+    else{
+        q->rear++;
+        q->Q[q->rear]=x;
+    }
+}
+
+int dequeue(struct queue *q){
+    int x=-1;
+    if(q->front==q->rear){
+        printf("queue is empty");
+    }
+    else{
+        q->front++;
+        x=q->Q[q->front];
+    }
+    return x;
+}
+
+void display(struct queue *q){
+    for(int i=q->front+1;i<=q->rear;i++){
+        printf("%d ",q->Q[i]);
+    }
+    printf("\n");
+}
+int main(){
+    struct queue q;
+    create(&q,5);
+
+    enqueue(&q,10);
+    enqueue(&q,20);
+    enqueue(&q,30);
+
+    display(&q);
+
+    printf("\n");
+
+    printf("%d ",dequeue(&q));
+
+    return 0;
+}
